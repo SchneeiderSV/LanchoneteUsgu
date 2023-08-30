2eafc24ef56cf6658831329b8287ce85567a1a5f
Schneeeiderツ <carlosmiguelschneider@gmail.com>
2023-08-29 22:49:19 -0300

    Nem sempre um novo começo é um recomeço quando se recicla matéria utilizada

---
+++ footer.php
@@ -0,0 +1,16 @@
+</body>
+    <div class="footerContainer">
+        <div class="footerItem">
+            <a href=""><img src="assets/house.png" width="35"></a>
+        </div>     
+        <div class="footerItem">
+            <a href=""><img src="assets/shopcart.png" width="35"></a>
+        </div>
+        <div class="footerItem">
+            <a href=""><img src="assets/historical.png" width="35"></a>
+        </div>
+        <div class="footerItem">
+            <a href=""><img src="assets/user.png" width="35"></a>
+        </div>
+    </div>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D'manos Merenderos</title>
+    <link rel="stylesheet" href="felipestyle.css">
+    <link rel="stylesheet" href="schneiderstyle.css">
+</head>
+<body>
+<div class="logo">
+    <img class="logo_icon" src="assets/picarone.jpeg" width="50px">
+</div>    
+
