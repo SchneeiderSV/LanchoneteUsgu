895f22d586f9977c80c541cac5a24f00a7a890f3
Carlos Schneider <carlos.schneider@CAMPUS.LABS>
2023-08-31 13:55:31 -0300

    aaa

--- footer.php
+++ footer.php
@@ -7,28 +7,28 @@
             <a href="index.php"><img src="assets/house.png" width="35"></a>
         </div> 
         <?php 
-            if(isset($_SESSION['id'])){ 
-                echo "<div class='footerItem'>";
-                    echo "<a href='cart.php'><img src='assets/shopcart.png' width='35'></a>";
-                echo "</div>";
-                echo "<div class='footerItem'>";
-                    echo "<a href='historical.php'><img src='assets/historical.png' width='35'></a>";
-                echo "</div>";
-                echo "<div class='footerItem'>";
-                    echo "<a href='user.php'><img src='assets/user.png' width='35'></a>";
-                echo "</div>";
-                echo "<div class='footerItem'>";
-                    echo "<a href='logout.php'><img src='assets/logout.png' width='35'></a>";
-                echo "</div>";
-                if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){
-                    echo "<div class='footerItem'>";
-                        echo "<a href='settings.php'><img src='assets/settings.png' width='35'></a>";
-                    echo "</div>";
+            if(isset($_SESSION['id'])){ ?>
+                <div class='footerItem'>
+                    <a href='cart.php'><img src='assets/shopcart.png' width='35'></a>
+                </div>
+                <div class='footerItem'>
+                    <a href='historical.php'><img src='assets/historical.png' width='35'></a>
+                </div>
+                <div class='footerItem'>
+                    <a href='user.php'><img src='assets/user.png' width='35'></a>
+                </div>
+                <div class='footerItem'>
+                    <a href='logout.php'><img src='assets/logout.png' width='35'></a>
+                </div> <?php
+                if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){ ?>
+                    <div class='footerItem'>
+                        <a href='settings.php'><img src='assets/settings.png' width='35'></a>
+                    </div> <?php
                 }
-            } else{
-                echo "<div class='footerItem'>";
-                    echo "<a href='login.php'><img src='assets/user.png' width='35'></a>";
-                echo "</div>";
+            } else{ ?>
+                <div class='footerItem'>;
+                    <a href='login.php'><img src='assets/user.png' width='35'></a>;
+                </div>; <?php
             }
         ?>
     </div>
