d3ce4076a295b4222ab5f1a38939403bbcbb8285
in-acio <inacio.sec2@gmail.com>
2023-09-08 12:14:50 -0300

    bebidas

--- lanchonete.sql
+++ lanchonete.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Set-2023 às 16:46
+-- Tempo de geração: 08-Set-2023 às 17:13
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.1
 
@@ -20,7 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `lanchonete`
 --
-
 CREATE DATABASE lanchonete;
 USE lanchonete;
 -- --------------------------------------------------------
@@ -48,7 +47,9 @@ INSERT INTO `dishes` (`id`, `name`, `price`, `description`, `size`, `is_drink`, 
 (1, 'Tacu Tacu', 60, 'O tacu-tacu é um prato tradicional do Peru que combina arroz e feijão. Os grãos são misturados e refogados com cebola, alho e pimentão. A mistura é compactada na frigideira até formar uma crosta dourada. ', 'Grande', 0, '64f9e03e7378b.jfif', '2023-09-07 11:37:50'),
 (2, 'Salchipapas', 40, 'A salchipapas é uma comida rápida e popular no Peru. É uma combinação das palavras \"salchicha\" (salsicha) e \"papas\" (batatas). O prato consiste em salsichas fritas e batatas fritas cortadas em palitos, servidas geralmente com molhos como ketchup, maionese', 'Médio', 0, '64f9e10a96d32.webp', '2023-09-07 11:41:14'),
 (3, 'Lomo Saltado', 80, 'O lomo saltado é um prato emblemático da culinária peruana que combina influências chinesas e peruanas. É conhecido por sua combinação única de sabores e texturas.', 'Grande', 0, '64f9e16a9edfc.jpg', '2023-09-07 11:42:50'),
-(4, 'Causa a la limeña', 20, 'A causa a la limeña é uma entrada fria e colorida. É uma deliciosa combinação de sabores suaves e picantes.\r\n', 'Pequeno', 0, '64f9e1e16a9e3.jpg', '2023-09-07 11:44:49');
+(4, 'Causa a la limeña', 20, 'A causa a la limeña é uma entrada fria e colorida. É uma deliciosa combinação de sabores suaves e picantes.\r\n', 'Pequeno', 0, '64f9e1e16a9e3.jpg', '2023-09-07 11:44:49'),
+(5, 'Pisco Sour', 15, 'O Pisco Sour é um coquetel emblemático do Peru, feito à base de pisco, uma aguardente de uva típica da região. Leva pisco, suco de limão, xarope de açúcar, clara de ovo e algumas gotas de amargo de Angostura. É agitado vigorosamente para criar uma espuma ', '500 ml', 1, '64fb39c31a0da.jfif', '2023-09-08 12:12:03'),
+(6, 'Chilcano', 18, 'O Chilcano é feito com pisco e ginger ale (ou cerveja de gengibre), e é geralmente servido com gelo e uma fatia de limão ou lima. A bebida tem um sabor levemente adocicado e efervescente, com uma nota picante do ginger ale que contrasta bem com a suavidad', '500 ml', 1, '64fb39ed1c761.jpg', '2023-09-08 12:12:45');
 
 -- --------------------------------------------------------
 
@@ -92,7 +93,9 @@ INSERT INTO `dishes_ingredients` (`id`, `quantity`, `dish_id`, `ingredient_id`, 
 (21, 1, 4, 7, '2023-09-07 11:44:49'),
 (22, 1, 4, 11, '2023-09-07 11:44:49'),
 (23, 1, 4, 15, '2023-09-07 11:44:49'),
-(24, 1, 4, 16, '2023-09-07 11:44:50');
+(24, 1, 4, 16, '2023-09-07 11:44:50'),
+(25, 1, 5, 17, '2023-09-08 12:12:03'),
+(26, 1, 6, 18, '2023-09-08 12:12:45');
 
 -- --------------------------------------------------------
 
@@ -112,22 +115,24 @@ CREATE TABLE `ingredients` (
 --
 
 INSERT INTO `ingredients` (`id`, `name`, `quantity`, `created_at`) VALUES
-(1, 'Feijão canário', 100, '2023-09-07 11:31:51'),
-(2, 'Arroz', 100, '2023-09-07 11:32:07'),
-(3, 'Cebola roxa', 150, '2023-09-07 11:32:16'),
-(4, 'Alho', 200, '2023-09-07 11:32:25'),
-(5, 'Pimenta-ají amarela', 150, '2023-09-07 11:32:37'),
-(6, 'Pimenta', 300, '2023-09-07 11:32:49'),
-(7, 'Batata', 300, '2023-09-07 11:33:14'),
+(1, 'Feijão canário', 99, '2023-09-07 11:31:51'),
+(2, 'Arroz', 99, '2023-09-07 11:32:07'),
+(3, 'Cebola roxa', 147, '2023-09-07 11:32:16'),
+(4, 'Alho', 199, '2023-09-07 11:32:25'),
+(5, 'Pimenta-ají amarela', 147, '2023-09-07 11:32:37'),
+(6, 'Pimenta', 297, '2023-09-07 11:32:49'),
+(7, 'Batata', 299, '2023-09-07 11:33:14'),
 (8, 'Salsicha', 300, '2023-09-07 11:33:19'),
 (9, 'Ketchup', 100, '2023-09-07 11:33:29'),
 (10, 'Mostarda', 100, '2023-09-07 11:33:35'),
-(11, 'Maionese', 100, '2023-09-07 11:33:40'),
+(11, 'Maionese', 99, '2023-09-07 11:33:40'),
 (12, 'Carne bovina', 200, '2023-09-07 11:33:49'),
 (13, 'Molho de soja', 100, '2023-09-07 11:33:57'),
 (14, 'Tomate', 300, '2023-09-07 11:34:04'),
-(15, 'Abacate', 150, '2023-09-07 11:35:09'),
-(16, 'Frango', 200, '2023-09-07 11:35:20');
+(15, 'Abacate', 148, '2023-09-07 11:35:09'),
+(16, 'Frango', 198, '2023-09-07 11:35:20'),
+(17, 'Pisco Sour', 50, '2023-09-08 12:11:00'),
+(18, 'Chilcano', 50, '2023-09-08 12:11:12');
 
 -- --------------------------------------------------------
 
@@ -240,31 +245,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `dishes`
 --
 ALTER TABLE `dishes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `dishes_ingredients`
 --
 ALTER TABLE `dishes_ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de tabela `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `orders_dishes`
 --
 ALTER TABLE `orders_dishes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `users`
